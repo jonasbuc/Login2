3295ccd54b8179b384606e200f13a6837526539f
jonasbuchner <155025571+jonasbuc@users.noreply.github.com>
2024-05-02 09:08:45 +0200

    Initial commit

---
+++ Login2/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Login2.Pages.Account
+{
+	public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential credential { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            //Verify Credentials
+            if (credential.Username == "admin" && credential.Password == "Password")
+            {
+                //security context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin")
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal();
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Methodepage");
+            }
+            return Page();
+        }
+
+        public class Credential
+        {
+            [Required]
+            [Display(Name = "User Name")]
+            public string Username { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+    }
+}
---
+++ Login2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Login2;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+
+        var app = builder.Build();
+
+        static void Configureservices(IServiceCollection services)
+        {
+            services.AddAuthentication().AddCookie("MyCookieAuth", options => { options.Cookie.Name = "MyCookieAuth"; });
+
+            services.AddRazorPages();
+        }
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+        }
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
+}
+
